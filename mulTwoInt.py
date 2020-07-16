defdaae0b7da61584d124ae9e0aa60a1730dac39
MadarH <madar@madar-VirtualBox>
2020-07-16 13:39:19 +0400

    git commit -m "Multiplication de deux variables"

---
+++ mulTwoInt.py
@@ -0,0 +1,10 @@
+print("Veuillez entrer deux nombres pour la multiplication:")
+def mul(a, b):
+	multi = a * b
+	return multi
+
+a=int(input())
+b=int(input())
+
+print("La reponse est:")
+print(mul(a, b))
