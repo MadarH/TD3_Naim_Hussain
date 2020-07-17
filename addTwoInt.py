41111d8c673545e50e6f6dd88879d40c73e01544
naim833 <naim.bhunnoo@gmail.co>
2020-07-17 09:00:32 +0400

    0.1

---
+++ addTwoInt.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+__name__ = "__main__"
+
+def add(a, b):
+    print(a+b)
+   
+
+if __name__ ==  "__main__":
+    print('Enter first number:')
+    a = input()
+    print('Enter second number:')
+    b = input()
+    add(a,b)
+
+
+
+
+
