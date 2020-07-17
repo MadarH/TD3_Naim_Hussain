b42ead3556daf97d6902656648bd7c98e65381ca
MadarH <madar@madar-VirtualBox>
2020-07-17 09:53:23 +0400

    git tag -a "tag 0.2" -m "Modifications pour numerer les arguments"

--- mulTwoInt.py
+++ mulTwoInt.py
@@ -6,5 +6,16 @@ a = int( sys.argv[1] )
 b = int( sys.argv[2] )
 
 
+if(len(sys.argv)) == 3:
+	print("Le nombre d'arguments est:"), (len(sys.argv))
+	
+else:
+	print("Erreur, le nombre d'arguments est:"), (len(sys.argv))
+	sys.exit(0)
+
+
 print("La reponse est:")
 print(a*b)
+
+
+
