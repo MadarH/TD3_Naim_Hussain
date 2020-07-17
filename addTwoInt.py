df4e3686263aaf45a2e8d0a864dd664ba6005603
naim833 <naim.bhunnoo@gmail.co>
2020-07-17 09:52:02 +0400

    0.2

--- addTwoInt.py
+++ addTwoInt.py
@@ -2,6 +2,10 @@
 import sys
 
 def add():
+    print("Nombre d'argument : " +str(len(sys.argv)-1))
+    if len(sys.argv) != 3:
+       print("Error.Please run the program again with 2 arguments")
+       sys.exit(0)
     a = int( sys.argv[1] )
     b = int( sys.argv[2] )
     print(a+b)
