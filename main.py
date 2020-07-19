f8e873ff2e9a70bd6e7bf1c89617f1e2a4f4beca
naim833 <naim.bhunnoo@gmail.co>
2020-07-19 17:51:23 +0400

    1.0

--- main.py
+++ main.py
@@ -9,3 +9,32 @@ if (a ==1):
    add()
 elif(a == 0):
    sys.exit(0)
+#!/usr/bin/env python
+
+#addition
+import sys
+from addTwoInt import add
+print("Voulez vous additioner deux valeurs : ")
+print("1 = oui ")
+print("0 = non")
+
+print("Votre choix: ")
+a = input()
+if (a ==1):
+   add()
+elif(a == 0):
+   print("Proceder a la multiplication.")
+
+
+#multiplication
+from TwoIntFunc import mul
+print("Voulez vous multiplier deux valeurs : ")
+print("1 = oui ")
+print("0 = non")
+
+print("Votre choix: ")
+a = input()
+if (a == 1):
+   mul()
+elif(a == 0):
+   sys.exit(0)
